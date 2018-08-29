3b5e8124cf68f87949a5259d3d1c3efdc19e9211
Douglas Bezerra Possas <douglasbpossas@gmail.com>
2018-08-29 06:18:45 -0300

    Permitindo o bind do v-model

--- src/components/VAInputGroup.vue
+++ src/components/VAInputGroup.vue
@@ -15,15 +15,13 @@
       <slot name="slotPrefixAddonText"></slot>
     </span>
     <va-input
-      :ref="input"
-      :type="type"
-      :vaId="vaId"
-      :placeholder="placeholder"
-      :size="size"
-      :value="value"
-      :isFormControl="type!='file'"
-      :isDisabled="isDisabled"
-      v-on:input="updateValue($event.target.value)"
+        :type="type"
+        :vaId="vaId"
+        :placeholder="placeholder"
+        :size="size"
+        :value="value"
+        :isFormControl="type!='file'"
+        :isDisabled="isDisabled"
     ></va-input>
     <span v-if="suffixAddonText || $slots.slotSuffixAddonText" class="input-group-addon">
       {{ suffixAddonText }}
@@ -81,13 +79,6 @@ export default {
     isDisabled: {
       type: Boolean,
       default: false
-    }
-  },
-  methods: {
-    updateValue: function (value) {
-      this.$refs.input.value = value
-      
-      this.$emit('input', value)
     }
   },
   computed: {
